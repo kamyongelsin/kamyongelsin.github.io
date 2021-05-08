72e7f742f804c2a44cef3decacfc1501368fa802
Furkan Tolga Yüce <yucefurkantolga@gmail.com>
2021-05-08 22:42:59 +0300

    logo

--- src/Navbar.js
+++ src/Navbar.js
@@ -7,7 +7,9 @@ function Navbar() {
                 <div class="container">
                 <header class="d-flex flex-wrap align-items-center justify-content-center justify-content-md-between py-3 mb-4 border-bottom">
                     <a href="/" class="d-flex align-items-center col-md-3 mb-2 mb-md-0 text-white text-decoration-none">
-                        <span class="fs-4">Kamyongelsin.com</span>
+                        <span class="fs-2">
+                            <i class="fas fa-truck-moving"></i> KAMYON GELSİN
+                        </span>
                     </a>
 
                     <ul class="nav col-12 col-md-auto mb-2 justify-content-center mb-md-0">
