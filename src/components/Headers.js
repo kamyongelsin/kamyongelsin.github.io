edcf839e72825a39cf81cbcb364b900f47a0f5fd
Furkan Tolga Yüce <yucefurkantolga@gmail.com>
2021-07-27 23:33:43 +0300

    Logo değişimi

--- src/components/Headers.js
+++ src/components/Headers.js
@@ -1,4 +1,5 @@
 import React, { useState} from 'react';
+import logo from '../figures/kg-logo.png';
 
 function Headers() {
 
@@ -15,16 +16,8 @@ function Headers() {
 
     return (
         <React.Fragment>
-            <div className={colorChange ? 'px-4 pt-3 text-center border-bottom sticky-top bg-primary text-white' : 'px-4 pt-3 text-center border-bottom sticky-top bg-white'}>
-                <h1 className="display-4 fw-bold">
-                    <span className={colorChange ? '' : 'text-primary'}>
-                        kamyongelsin.com
-                    </span>
-                </h1>
-                
-                <div className="lead fst-italic mb-2">
-                    "Bir tıkla Türkiye'nin her yerinde kamyon kapında."
-                </div>
+            <div className="px-4 pt-3 text-center border-bottom sticky-top bg-white">
+                <img src={logo} alt="Logo" style={{height:92}}/>
             </div>
 
         </React.Fragment>
